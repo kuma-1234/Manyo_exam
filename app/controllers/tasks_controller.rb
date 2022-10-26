8045ffdc1e3a795bef0efa5ec6920e6f8e07ef12
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-10-26 13:38:09 +0900

    index/showの実装の追加。view/controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,32 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスクを作成しました！"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+
+
+  private
+
+  def task_params
+    params.require(:task).permit(:task_title, :task_content)
+  end
+
 end
