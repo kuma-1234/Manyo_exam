03702c5cd8e0557a4aebbf1a1b97d1cd84bcbdb9
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-10-30 09:55:12 +0900

    indexの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_29_160309) do
+ActiveRecord::Schema.define(version: 2022_10_30_005317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_10_29_160309) do
     t.date "deadline", default: -> { "CURRENT_DATE" }, null: false
     t.string "priority"
     t.integer "status", default: 0
+    t.index ["task_title"], name: "index_tasks_on_task_title"
   end
 
 end
