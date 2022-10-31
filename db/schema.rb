60314cf849311144a2936396618366c080b34ec8
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-10-31 09:56:46 +0900

    priorityのデータ型を整数に変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_30_005317) do
+ActiveRecord::Schema.define(version: 2022_10_31_005547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(version: 2022_10_30_005317) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.date "deadline", default: -> { "CURRENT_DATE" }, null: false
-    t.string "priority"
     t.integer "status", default: 0
+    t.integer "priority", default: 0
     t.index ["task_title"], name: "index_tasks_on_task_title"
   end
 
