c87457e6b8adbedc352a19dd6fac2f269870defa
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-11-01 11:23:22 +0900

    テーブルのカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_31_005547) do
+ActiveRecord::Schema.define(version: 2022_11_01_022018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2022_10_31_005547) do
     t.integer "status", default: 0
     t.integer "priority", default: 0
     t.index ["task_title"], name: "index_tasks_on_task_title"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
