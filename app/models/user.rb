47cd7e4396a0d6d8872381b0a0777e35406cd28d
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-11-01 22:32:12 +0900

    編集時はパスワードの入力がなくても更新できるように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, on: :new
   has_many :tasks, dependent: :destroy
 end
