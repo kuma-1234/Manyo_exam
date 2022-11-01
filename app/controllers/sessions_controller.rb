75e9ad89368c486987e11e36e0d5d21f055187d2
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-11-01 14:33:42 +0900

    ログイン機能とコントローラーによる制限の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,14 @@
 class SessionsController < ApplicationController
   skip_before_action :login_required, only:[ :new, :create ]
-  
+
   def new
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      redirect_to user_path(user.id)
+      redirect_to user_path(user)
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session.delete(:user.id)
+    session.delete(:user_id)
     flash[:notice] = 'ログアウトしました'
     redirect_to new_session_path
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,20 +2,29 @@ class UsersController < ApplicationController
   skip_before_action :login_required, only:[ :new, :create ]
 
   def new
-    @user = User.new
+    if logged_in?
+      redirect_to tasks_path
+    else
+      @user = User.new
+    end
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      #redirect_to
+      flash.now[:notice] = 'アカウントを作成しました！'
+      session[:user_id] = @user.id
+      redirect_to task_path(@user), notice: 'ログインしました！'
     else
       render :new
     end
   end
 
   def show
-    @user = User.find(params[:id])
+      @user = User.find(params[:id])
+      unless @user == current_user
+        redirect_to tasks_path, notice:'ログインIDとアクセスしようとしているIDが一致しません。'
+    end
   end
 
 
