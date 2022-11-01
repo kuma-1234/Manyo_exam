5253ae0b6fc23d7377a7f79355c0f9a379ae2b2c
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-11-01 11:40:16 +0900

    user/validateの追加

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2022_11_01_022018) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
