a1e31789519b5f0bf79df086d3cca045d8ebb7c2
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-11-04 22:16:16 +0900

    seedデータの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,43 @@ User.create!(
   password_confirmation: 'test1234',
   admin: true
 )
+
+Task.create!(
+  task_title: "title",
+    task_content: "contentcontent",
+    deadline: "2022-12-24",
+    status: 2,
+    priority: 3,
+    user_id: 1
+)
+
+# 5.times do |num|
+#     User.create!(
+#     email: "test#{num}@sample.com",
+#     name: "user#{num}",
+#     password: 'test321',
+#     password_confirmation: 'test321',
+#     admin: false
+#   )
+# end
+
+# User.all.each do |user|
+#   user.tasks.create!(
+#     task_title: "title",
+#     task_content: "content",
+#     deadline: Date,
+#     status: rand(1..3),
+#     priority: rand(1..3)
+#   )
+# end
+
+# 5.times do |index|
+#   Task.create!(
+#     task_title: "title#{index}",
+#     task_content: "content#{index}",
+#     deadline: Date,
+#     status: rand(1..3),
+#     priority: rand(1..3),
+#     user: @user
+#   )
+# end
