2df8d8359c805d3c8681dc43b7f7b411aa209a30
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-11-04 13:11:10 +0900

    userのFactoryBotの作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
   factory :user do
-    
+    name { 'user1' }
+    email { 'user@test.com' }
+    password { 'test123456' }
+    admin { 'false' }
+  end
+  factory :user2, class: User do
+    name { 'user2' }
+    email { 'user2@test.com' }
+    password { 'test123456' }
+    admin { 'false' }
+  end
+  factory :admin_user, class: User do
+    name { 'admin_user' }
+    email { 'admin_user@test.com' }
+    password { 'test123456' }
+    admin { 'true' }
   end
 end
