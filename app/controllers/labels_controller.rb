5aaa63c5aec685371383d6b70f25297428ec6e4e
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-11-06 21:45:54 +0900

    labelのコントローラー作成

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,2 +1,44 @@
 class LabelsController < ApplicationController
+
+  def index
+    @labels = Label.all
+  end
+
+  def new
+    @label = Label.new
+  end
+
+  def create
+    @label = Label.new
+      if @label.save(params_label)
+        redirect_to labels_path
+      else
+        render :new
+      end
+  end
+
+  def edit
+    @label = Label.find(params[:id])
+  end
+
+  def update
+    @label = Label.find(params[:id])
+    if @label.update(params_label)
+      redirect_to labels_path, notice: '更新しました！'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @label = Label.find(params[:id])
+    @label.destroy
+    redirect_to labels_path
+  end
+
+  private
+  
+  def params_label
+    params.require(:label).permit(:label_name)
+  end
 end
